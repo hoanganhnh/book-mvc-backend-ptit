6042697d9c527c01e597cea98f2c4622d03afbd9
hoanganh <hoahoanganh20012001@gmail.com>
2022-10-29 22:14:48 +0700

    refactor: clean app module

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,23 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'chore',
+        'style',
+        'refactor',
+        'ci',
+        'test',
+        'perf',
+        'revert',
+        'deploy', // for deploy
+        'config', // for config app
+      ],
+    ],
+  },
+};
